90137f4ffa25f2b1b53a60b78eb3442ff1f6e3b9
tianshan20081 <zhangshch2008@gmail.com>
2013-11-05 20:00:36 +0800

    resource

---
+++ src/com/aoeng/oa/model/ActionMethodOper.java
@@ -0,0 +1,77 @@
+/**
+ * 
+ */
+package com.aoeng.oa.model;
+
+/**
+ * Nov 5, 20137:51:43 PM
+ * 
+ */
+public class ActionMethodOper {
+	/**
+	 * 操作的唯一标识 eg:ADD，UPDATE，
+	 */
+	private String operSn;
+	/**
+	 * 操作存储 aclstate在 ACL 当中的 索引位置，索引必须大于0，并且 小于等于 31 同一个资源当中索引值都是唯一的，不能重复
+	 */
+	private int operIndex;
+	/**
+	 * 对应的方法名字，同一个操作可能对应多个方法 eg:addInput|add,apdateInput|update,del
+	 */
+	private String methodName;
+	/**
+	 * 操作的名称
+	 */
+	private String operName ;
+	/**
+	 * @return the operSn
+	 */
+	public String getOperSn() {
+		return operSn;
+	}
+	/**
+	 * @param operSn the operSn to set
+	 */
+	public void setOperSn(String operSn) {
+		this.operSn = operSn;
+	}
+	/**
+	 * @return the operIndex
+	 */
+	public int getOperIndex() {
+		return operIndex;
+	}
+	/**
+	 * @param operIndex the operIndex to set
+	 */
+	public void setOperIndex(int operIndex) {
+		this.operIndex = operIndex;
+	}
+	/**
+	 * @return the methodName
+	 */
+	public String getMethodName() {
+		return methodName;
+	}
+	/**
+	 * @param methodName the methodName to set
+	 */
+	public void setMethodName(String methodName) {
+		this.methodName = methodName;
+	}
+	/**
+	 * @return the operName
+	 */
+	public String getOperName() {
+		return operName;
+	}
+	/**
+	 * @param operName the operName to set
+	 */
+	public void setOperName(String operName) {
+		this.operName = operName;
+	}
+	
+	
+}
--- src/com/aoeng/oa/model/ActionResource.java
+++ src/com/aoeng/oa/model/ActionResource.java
@@ -3,13 +3,33 @@
  */
 package com.aoeng.oa.model;
 
+import java.util.Map;
+
 /**
  * Nov 4, 20137:26:40 PM
  * 
  */
 public class ActionResource implements SysResource {
 	private int id;
+	/**
+	 * 资源组织机构名称
+	 */
+	private String name;
+	/**
+	 * 资源对应的 action 类名，可以有多个(partyAction ,CompanyAction,用竖线分割)
+	 */
 	private String className;
+	/**
+	 * 资源的唯一标识（company,department,）
+	 */
+	private String sn;
+	/**
+	 * 资源的排序号
+	 */
+	private int orderNumber;
+	
+	private Map<String, ActionMethodOper> opers ;
+	
 	/**
 	 * @return the id
 	 */
