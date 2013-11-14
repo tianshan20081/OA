2a8a6db8646ce14042a2e3f2ed857225503c6588
tianshan20081 <zhangshch2008@gmail.com>
2013-11-14 16:37:33 +0800

    1.用户登录模块。数据保存对象(LoginInfoVo，里面可以保存登录用户的信息:id,username,name,ip,loginTime,...) 2.保存用户登录数据对象到Session 模块。ServletApplicationContent.getRequest().setAttribute("login",loginInfoVo); 3.用户登录页面，index.jsp; 4.需要添加的操作：用户操作验证，检查Session 中是否有用户信息。 5.用户注销功能（ServletApplicationContent.getRequest().getSession().invocialtion();销毁Session）

---
+++ src/com/aoeng/oa/vo/LoginInfoVo.java
@@ -0,0 +1,82 @@
+/**
+ * 
+ */
+package com.aoeng.oa.vo;
+
+import java.util.Date;
+
+/**
+ * Nov 14, 201312:10:22 PM
+ *
+ */
+public class LoginInfoVo
+{
+	private String username ;
+	private String name ;
+	private int id ;
+	private String ip ;
+	private Date loginTime ;
+	/**
+	 * @return the username
+	 */
+	public String getUsername() {
+		return username;
+	}
+	/**
+	 * @param username the username to set
+	 */
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+	/**
+	 * @return the ip
+	 */
+	public String getIp() {
+		return ip;
+	}
+	/**
+	 * @param ip the ip to set
+	 */
+	public void setIp(String ip) {
+		this.ip = ip;
+	}
+	/**
+	 * @return the loginTime
+	 */
+	public Date getLoginTime() {
+		return loginTime;
+	}
+	/**
+	 * @param loginTime the loginTime to set
+	 */
+	public void setLoginTime(Date loginTime) {
+		this.loginTime = loginTime;
+	}
+	
+	
+
+}
---
+++ src/com/aoeng/oa/web/action/LoginAction.java
@@ -0,0 +1,79 @@
+/**
+ * 
+ */
+package com.aoeng.oa.web.action;
+
+import java.util.Date;
+
+import javax.annotation.Resource;
+
+import org.apache.struts2.ServletActionContext;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+
+import com.aoeng.oa.model.User;
+import com.aoeng.oa.service.UserService;
+import com.aoeng.oa.vo.LoginInfoVo;
+
+/**
+ * Nov 14, 201311:57:46 AM
+ * 
+ */
+@Controller("loginAction")
+@Scope("prototype")
+public class LoginAction
+{
+	@Resource
+	private UserService userService;
+	private String username;
+	private String password;
+
+	public String execute() {
+		User user =userService.login(username,password);
+		LoginInfoVo loginInfoVo = new LoginInfoVo();
+		loginInfoVo.setId(user.getId());
+		loginInfoVo.setUsername(user.getUsername());
+		loginInfoVo.setName(user.getPerson().getName());
+		loginInfoVo.setIp(ServletActionContext.getRequest().getRemoteHost());
+		loginInfoVo.setLoginTime(new Date());
+		
+		ServletActionContext.getRequest().setAttribute("login", loginInfoVo);
+		return "back_index";
+	}
+
+	public String quit() {
+		ServletActionContext.getRequest().getSession().invalidate();
+		return "login";
+	}
+
+	/**
+	 * @return the username
+	 */
+	public String getUsername() {
+		return username;
+	}
+
+	/**
+	 * @param username
+	 *            the username to set
+	 */
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+
+	/**
+	 * @param password
+	 *            the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
